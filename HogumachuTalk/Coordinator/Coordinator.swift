6ff59fe8264fd5526afed4be4a7dc9efd629d824
hogumachu <castle_jun@daum.net>
2021-11-10 11:34:56 +0900

    Scene Transition (SignIn)

--- HogumachuTalk/Coordinator/Coordinator.swift
+++ HogumachuTalk/Coordinator/Coordinator.swift
@@ -5,24 +5,48 @@ class Coordinator {
         let mainNavigationController: UINavigationController
         let loginViewControllerFactory: () -> LoginViewController
         let signUpViewControllerFactory: () -> SignUpViewController
+        
+        let homeTabBarController: UITabBarController
+        
+        let profileNavigationController: UINavigationController
         let profileViewControllerFactory: () -> ProfileViewController
+        
+        let chatNavigationController: UINavigationController
         let chatViewControllerFactory: () -> ChatViewController
+        
+        let settingNavigationController: UINavigationController
         let settingViewControllerFactory: () -> SettingViewController
     }
     
     let mainNavigationController: UINavigationController
     let loginViewControllerFactory: () -> LoginViewController
     let signUpViewControllerFactory: () -> SignUpViewController
+    
+    let homeTabBarController: UITabBarController
+    
+    let profileNavigationController: UINavigationController
     let profileViewControllerFactory: () -> ProfileViewController
+    
+    let chatNavigationController: UINavigationController
     let chatViewControllerFactory: () -> ChatViewController
+    
+    let settingNavigationController: UINavigationController
     let settingViewControllerFactory: () -> SettingViewController
     
     init(dependency: Dependency) {
         self.mainNavigationController = dependency.mainNavigationController
         self.loginViewControllerFactory = dependency.loginViewControllerFactory
         self.signUpViewControllerFactory = dependency.signUpViewControllerFactory
+        
+        self.homeTabBarController = dependency.homeTabBarController
+        
+        self.profileNavigationController = dependency.profileNavigationController
         self.profileViewControllerFactory = dependency.profileViewControllerFactory
+        
+        self.chatNavigationController = dependency.chatNavigationController
         self.chatViewControllerFactory = dependency.chatViewControllerFactory
+        
+        self.settingNavigationController = dependency.settingNavigationController
         self.settingViewControllerFactory = dependency.settingViewControllerFactory
     }
 }
@@ -42,5 +66,42 @@ extension Coordinator {
         vc.coordinator = self
         
         mainNavigationController.present(vc, animated: true, completion: nil)
+    }
+    
+    func signIn() {
+        let profileVC = profileViewControllerFactory()
+        profileVC.coordinator = self
+        profileVC.tabBarItem = UITabBarItem(title: "프로필",
+                                            image: UIImage(systemName: "person"),
+                                            selectedImage: UIImage(systemName: "person.fill")
+        )
+        profileNavigationController.setViewControllers([profileVC], animated: false)
+        
+        let chatVC = chatViewControllerFactory()
+        chatVC.coordinator = self
+        chatVC.tabBarItem = UITabBarItem(title: "채팅",
+                                         image: UIImage(systemName: "quote.bubble"),
+                                         selectedImage: UIImage(systemName: "quote.bubble.fill")
+        )
+        chatNavigationController.setViewControllers([chatVC], animated: false)
+        
+        let settingVC = settingViewControllerFactory()
+        settingVC.coordinator = self
+        settingVC.tabBarItem = UITabBarItem(title: "설정",
+                                            image: UIImage(systemName: "gearshape"),
+                                            selectedImage: UIImage(systemName: "gearshape.fill")
+        )
+        settingNavigationController.setViewControllers([settingVC], animated: false)
+        
+        homeTabBarController.setViewControllers(
+            [
+                profileNavigationController,
+                chatNavigationController,
+                settingNavigationController
+            ],
+            animated: false
+        )
+        
+        mainNavigationController.pushViewController(homeTabBarController, animated: true)
     }
 }
--- HogumachuTalk/DI/CompositionRoot.swift
+++ HogumachuTalk/DI/CompositionRoot.swift
@@ -19,22 +19,47 @@ extension AppDependency {
             return .init(dependency: .init(viewModel: .init()))
         }
         
+        let homeTabBarController: UITabBarController = {
+            let tabBarController = UITabBarController()
+            return tabBarController
+        }()
+        
+        let profileNavigationController: UINavigationController = {
+            let navi = UINavigationController()
+            return navi
+        }()
+        
         let profileViewControllerFactory: () -> ProfileViewController = {
             return .init(dependency: .init(viewModel: .init()))
         }
+        
+        let chatNavigationController: UINavigationController = {
+            let navi = UINavigationController()
+            return navi
+        }()
         
         let chatViewControllerFactory: () -> ChatViewController = {
             return .init(dependency: .init(viewModel: .init()))
         }
         
+        let settingNavigationController: UINavigationController = {
+            let navi = UINavigationController()
+            return navi
+        }()
+        
         let settingViewControllerFactory: () -> SettingViewController = {
             return .init(dependency: .init(viewModel: .init()))
         }
+        
         return .init(coordinator: .init(dependency: .init(mainNavigationController: mainNavigationController,
                                                           loginViewControllerFactory: loginViewControllerFactory,
                                                           signUpViewControllerFactory: signUpViewControllerFactory,
+                                                          homeTabBarController: homeTabBarController,
+                                                          profileNavigationController: profileNavigationController,
                                                           profileViewControllerFactory: profileViewControllerFactory,
+                                                          chatNavigationController: chatNavigationController,
                                                           chatViewControllerFactory: chatViewControllerFactory,
+                                                          settingNavigationController: settingNavigationController,
                                                           settingViewControllerFactory: settingViewControllerFactory
                                                          )))
     }
