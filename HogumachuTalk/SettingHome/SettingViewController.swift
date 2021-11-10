58a068ef2b52b278f38b606868d2ba1c7ef712d4
hogumachu <castle_jun@daum.net>
2021-11-10 21:48:30 +0900

    Add LogOut Button, Method

--- HogumachuTalk/SettingHome/SettingViewController.swift
+++ HogumachuTalk/SettingHome/SettingViewController.swift
@@ -5,7 +5,14 @@ class SettingViewController: UIViewController {
         let viewModel: SettingViewModel
     }
     let viewModel: SettingViewModel
-    
+    private lazy var logOutButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("로그아웃", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.addTarget(self, action: #selector(logOutButtonDidTap), for: .touchUpInside)
+        return button
+    }()
     init(dependency: Dependency) {
         self.viewModel = dependency.viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,5 +29,18 @@ class SettingViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
+        
+        view.addSubview(logOutButton)
+        
+        // TODO: - Layout
+        NSLayoutConstraint.activate([
+            logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc
+    private func logOutButtonDidTap() {
+        viewModel.logOut()
     }
 }
--- HogumachuTalk/SettingHome/SettingViewModel.swift
+++ HogumachuTalk/SettingHome/SettingViewModel.swift
@@ -1,5 +1,14 @@
 class SettingViewModel: ViewModelType {
     var coordinator: Coordinator?
     
-    
+    func logOut() {
+        FirebaseImp.shared.signOut { [weak self] result in
+            switch result {
+            case .success(_):
+                self?.coordinator?.signOut()
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
 }
