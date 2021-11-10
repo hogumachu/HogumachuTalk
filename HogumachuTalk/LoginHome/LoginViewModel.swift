beaf7ad1d1e586b27ea5cc987f4238e703f7e198
hogumachu <castle_jun@daum.net>
2021-11-10 11:35:30 +0900

    LogIn Method (TODO: Completion)

--- HogumachuTalk/LoginHome/LoginViewModel.swift
+++ HogumachuTalk/LoginHome/LoginViewModel.swift
@@ -1,3 +1,12 @@
 class LoginViewModel {
-    
+    func logIn(email: String, password: String) {
+        if email.isEmpty || password.isEmpty {
+            // TODO: - 텍스트필드 다 채우라는 Alert
+            return
+        }
+        FirebaseImp.shared.signIn(email: email,
+                                  password: password) { result in
+            // TODO: - Completion
+        }
+    }
 }
