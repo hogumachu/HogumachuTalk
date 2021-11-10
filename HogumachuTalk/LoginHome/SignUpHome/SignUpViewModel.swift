439d3c80ce77d2d0d46a3ea12b33260e4436ee79
hogumachu <castle_jun@daum.net>
2021-11-10 11:36:41 +0900

    TODO: Change print -> alert

--- HogumachuTalk/LoginHome/SignUpHome/SignUpViewModel.swift
+++ HogumachuTalk/LoginHome/SignUpHome/SignUpViewModel.swift
@@ -1,3 +1,23 @@
 class SignUpViewModel {
-    
+    func signUp(email: String, password: String, confirmPassword: String, userName: String) {
+        // TODO: - 조건 분기 처리
+        if email.isEmpty || password.isEmpty || confirmPassword.isEmpty || userName.isEmpty {
+            print("TextField 를 다 채워야 함 !")
+            return
+        }
+        
+        if password != confirmPassword {
+            print("비밀번호가 동일해야 함 !")
+            return
+        }
+        
+        FirebaseImp.shared.signUp(email: email, password: password, userName: userName) { result in
+            switch result {
+            case .success(_):
+                print("회원가입 성공")
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
 }
