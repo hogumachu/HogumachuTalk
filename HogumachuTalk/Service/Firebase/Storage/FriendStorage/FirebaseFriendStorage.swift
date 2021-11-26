c284e8a4bacac865b077dccd6a19aa833bd7fe30
hogumachu <castle_jun@daum.net>
2021-11-26 15:05:35 +0900

    Create SectionModel Observable

--- HogumachuTalk/Service/Firebase/Storage/FriendStorage/FirebaseFriendStorage.swift
+++ HogumachuTalk/Service/Firebase/Storage/FriendStorage/FirebaseFriendStorage.swift
@@ -2,20 +2,38 @@ import RxSwift
 import RxCocoa
 
 class FirebaseFriendStorage: FirebaseFriendStorageType {
+    struct Dependency {
+        let userStorage: FirebaseUserStorageType
+    }
+    
+    init(dependency: Dependency) {
+        self.userStorage = dependency.userStorage
+    }
+    
+    let userStorage: FirebaseUserStorageType
+    
     private lazy var friendSubject = BehaviorSubject<[User]>(value: mock)
     
     func friendObservable() -> Observable<[User]> {
         return friendSubject
     }
     
+    lazy var setionModelObservable = Observable.combineLatest(userStorage.userObservable(), friendObservable())
+        .map { user, friend -> [UserSectionModel] in
+            let userSectionModel = UserSectionModel(model: 0, items: [user])
+            let friendSectionModel = UserSectionModel(model: 1, items: friend)
+            
+            return [userSectionModel, friendSectionModel]
+        }
+    
     let mock: [User] = [
-        .init(id: "Test1", userName: "Test1", email: "Test1@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test2", userName: "Test2", email: "Test2@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test3", userName: "Test3", email: "Test3@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test4", userName: "Test4", email: "Test4@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test5", userName: "Test5", email: "Test5@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test6", userName: "Test6", email: "Test6@Test.com", profileImageURL: "", backgroundImageURL: ""),
-        .init(id: "Test7", userName: "Test7", email: "Test7@Test.com", profileImageURL: "", backgroundImageURL: "")
+        .init(identity: "Test1", userName: "Test1", email: "Test1@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test2", userName: "Test2", email: "Test2@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test3", userName: "Test3", email: "Test3@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test4", userName: "Test4", email: "Test4@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test5", userName: "Test5", email: "Test5@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test6", userName: "Test6", email: "Test6@Test.com", profileImageURL: "", backgroundImageURL: ""),
+        .init(identity: "Test7", userName: "Test7", email: "Test7@Test.com", profileImageURL: "", backgroundImageURL: "")
     ]
 
 }
