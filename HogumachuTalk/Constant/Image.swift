0829a688d554e616791f35d7e07ff5481fac0fd0
hogumachu <castle_jun@daum.net>
2021-11-28 13:35:17 +0900

    Create Image Constant

--- HogumachuTalk/Constant/Image.swift
+++ HogumachuTalk/Constant/Image.swift
@@ -4,6 +4,9 @@ public let _pencilCircleFill = UIImage(systemName: "pencil.circle.fill")?.withTi
 public let _pencilCircle = UIImage(systemName: "pencil.circle")?.withTintColor(.white, renderingMode: .alwaysOriginal)
 public let _messageFill = UIImage(systemName: "message.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
 public let _xMark = UIImage(systemName: "xmark")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+public let _xMarkCircleFill = UIImage(systemName: "xmark.circle.fill")?.withTintColor(.darkGray, renderingMode: .alwaysOriginal)
+
+public let _magnifyingglass = UIImage(systemName: "magnifyingglass")?.withTintColor(.black, renderingMode: .alwaysOriginal)
 
 public let _checkMarkSquare = UIImage(systemName: "checkmark.square")?.withTintColor(.systemBrown, renderingMode: .alwaysOriginal)
 public let _checkMarkSquareFill = UIImage(systemName: "checkmark.square.fill")?.withTintColor(.systemBrown, renderingMode: .alwaysOriginal)
